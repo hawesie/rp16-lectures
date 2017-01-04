84ce1b8d2f8b47b7c82e5f12e96c326e0cde4230
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2017-01-04 12:10:50 +0000

    Initial files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Robot Programming Lecture Code
+
+Code from Robot Programming lectures 2016/17
+
+You will also need: 
+ * https://github.com/hawesie/rp-shared
+ * https://github.com/hawesie/rp-utils
+
+Both should be NXT projects.
---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+import lejos.nxt.Button;
+import rp.systems.RobotProgrammingDemo;
+
+public class HelloWorld extends RobotProgrammingDemo {
+
+	@Override
+	public void run() {
+		System.out.println("Hello");
+		Button.waitForAnyPress();
+	}
+
+	public static void main(String[] args) {
+		Button.waitForAnyPress();
+		RobotProgrammingDemo demo = new HelloWorld();
+		demo.run();
+
+	}
+
+}
